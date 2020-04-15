1732ff8d412131adda89c5275632fd34904e700c
lenndachen <49460507+lenndachen@users.noreply.github.com>
2020-04-15 13:16:55 -0400

    add purchase
    add purchase is working on console

--- src/components/EnterDataTab/index.js
+++ src/components/EnterDataTab/index.js
@@ -1,26 +1,53 @@
 import React, {Component} from 'react';
 
+import "./EnterDataTab.css"
 export default class EnterDataTab extends Component{
     constructor(props){
         super(props);
-
+        // this.listOfData = this.listOfData.bind(this);
     this.state={
-        userInput:" "
+        price: " ",
+        date: "yyyy-MM-dd",
+        category: " ", 
     }
 }
 
+// sets the state of prices on input
 updateUserInput = (e) =>{
-    this.setState=({
-        userInput: e.target.value
+    this.setState({
+        [e.target.name]: e.target.value,
     })
+    console.log("price", e.target.value)
  }
+
+handleSubmit = () => {
+        const { price, date, category} = this.state;
+        const purchase = {price, date, category}
+        console.log("purchase", purchase);
+    this.props.submitButton(purchase)
+}
+
+
 
     render(){
         return(
             <div>
-                <input defaultValue={this.state.userInput} onChange={e => this.updateUserInput(e)}/>
+                {/* form userinputs items 
+                onSubmit={this.props.submitItemCount} */}
+                <form className="inputs">
+                    {/* this.props.delayedOutput */}
+                    <input type="price" pattern="[0-9]*" name="price" value={this.state.price} onChange={this.updateUserInput}/>
+                    <input type="date" name="date" value={this.state.date} onChange={this.updateUserInput}/>
+                    <select type="text" name="category" value={this.state.category} onChange={this.updateUserInput}>
+                        <option type="text" name="category" value="food">Eating Out</option>
+                        <option type="text" name="category" value="gas">Gas</option>
+                        <option type="text" name="category" value="entertainment">Entertainment</option>
+                        <option type="text" name="category" value="grocery">Grocery's</option>
+                        <option type="text" name="category" value="other">Other</option>
+                    </select>
+                    <button type="button" onClick={this.handleSubmit}>Submit</button>
+                </form>
             </div>
         )
     }
 }
-
--- src/containers/EnterData/EnterData.js
+++ src/containers/EnterData/EnterData.js
@@ -3,13 +3,77 @@ import React, {Component} from 'react'
 import Header from '../../components/HeaderComponent/Header.js';
 import HamburgerTab from '../../components/HamburgerTab/index';
 import EnterDataTab from "../../components/EnterDataTab/index";
+// import SearchForm from '../../components/SearchForm/index.js';
 class EnterData extends Component{
+    constructor(props){
+        super(props);
+            this.state={
+                // all expenses in container
+                allExpenses: [],
+                eachExpense: {},
+        }
+    }
+
+    // Submit function 
+    handleSubmit = (purchase) => {  
+        console.log("purchase 3", purchase)
+        let array = this.state.allExpenses;
+        array.push(purchase)
+        this.setState({
+            array: array,
+        })
+    console.log("array", array)
+    }
+
+    // useEffect = (e) => {
+    //     const value = e.target.value;
+    //     const timeout = setTimeout(() => {
+    //       this.setOutput(value);
+    //     }, 200);
+    //     return () => {
+    //       clearTimeout(timeout);
+    //     };
+    //   }
+
+    // setOutput = () => {
+    //    const array = this.state.allExpenses;
+    //    console.log("array", array)
+    // }
+      
+    // submitItems = () => {
+    //     const count = (this.state.array(0))
+    //     const count2 = (this.state.array[1])
+    //     const count3 = (this.state.array[2])
+    //     const count4 = [{...count, ...count2, ...count3}]
+    //     const count5 = count4.push(this.state.allExpenses)
+    //     console.log("count 5", count5)
+    // }
+
+    submitInput = () => {
+        return(
+            <input type="button" value="Submit" name="submit" onClick={this.handleSubmit}/>
+        )
+    }
+
+    // performSearch = (query) => {
+          
+    //   }
+ 
     render(){
+        console.log("purchase", this.state.allExpenses)
         return(
             <div>
-            <Header />
-            <HamburgerTab/>
-            <EnterDataTab />
+                <Header />
+                <HamburgerTab/>
+                <div>
+                    <EnterDataTab submitButton={this.handleSubmit}/>
+                </div>
+                {/* submitItemCount={this.submitItems()}  */}
+                {/* delayedUpdate={this.useEffect} */}
+                <div>
+                    {/* <SearchForm onSearch={this.performSearch} />   */}
+                </div>
+            
             </div>
         )
     }
